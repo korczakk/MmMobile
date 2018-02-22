a4197ed88d7c729fe68394bd9220a0951bfbec51
Kamil Kkorczak <kamil.korczak@gmail.com>
2018-02-22 23:00:14 +0100

    Połączenie z firebase

--- MmMobile/MmMobile/MmMobile/MainPage.xaml.cs
+++ MmMobile/MmMobile/MmMobile/MainPage.xaml.cs
@@ -1,17 +1,34 @@
-﻿using System;
+﻿using MmMobile.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+
 namespace MmMobile
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        ProductListViewModel vm = new ProductListViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            //zalogowanie do Firebase
+            string idToken = vm.FirebaseSignIn();
+            
+            //Pobranie danych z Firebase  
+            vm.GetData(idToken);
+
+            base.OnAppearing();
+        }
+
+
+    }
 }
---
+++ MmMobile/MmMobile/MmMobile/ViewModel/ProductListViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using FireBaseClient;
+using MmMaker.Model;
+
+namespace MmMobile.ViewModel
+{
+    public class ProductListViewModel
+    {
+        string _appKey = "AIzaSyBaQegiosq-yCEp1CdNsZ6dGiAhQgN8fgw";
+
+        public ObservableCollection<ExcelContent> mmContent { get; private set; }
+
+        /// <summary>
+        /// Pobiera dane o MM z Firebase
+        /// </summary>
+        /// <param name="idToken"></param>
+        public void GetData(string idToken)
+        {
+            FirebaseDatabase firebase = new FirebaseDatabase(idToken);
+
+            IEnumerable<ExcelContent> Content = firebase.GetData<ExcelContent>("");
+
+            mmContent = new ObservableCollection<ExcelContent>(mmContent); 
+        }
+
+        /// <summary>
+        /// Loguje się do Firebase i zwraca idToken
+        /// </summary>
+        /// <returns>Token</returns>
+        public string FirebaseSignIn()
+        {
+            FirebaseAuthentication auth = new FirebaseAuthentication(_appKey);
+
+            SignInResponse signinResult = auth.SignInWithEmail("kamil.korczak@gmail.com", "HPdj690");
+
+            return signinResult.idToken;
+        }
+    }
+}
